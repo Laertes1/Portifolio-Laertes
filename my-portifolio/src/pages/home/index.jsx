4314a78179847ea195a56d453884ecf570b7726d
Laertes1 <laertes.angelo@gmail.com>
2024-03-19 19:42:25 -0300

    fixed bugs of responsivity in the page home

--- my-portifolio/src/pages/home/index.jsx
+++ my-portifolio/src/pages/home/index.jsx
@@ -77,9 +77,9 @@ export default function Index() {
                   hashSpy={true}
                   offset={-80}
                   duration={500}
-                  className="navBar-p p4"
+                  className="buttonLinkBoxer"
                 >
-                  <button className="btn-contato">ENTRAR EM CONTATO</button>
+                  <button className="btn-contato">ENTRAR EM CONTATO </button>
                 </ScrollLink>
 
                 <Link to="/AboutMe" className="Link">
